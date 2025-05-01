f270823da8fa2313be1908ee86a16265b4d8ab05
Harag <harag@cortexx.net>
2025-05-01 16:40:50 +0200

    minor cleanup

--- control.lua
+++ control.lua
@@ -1,7 +1,7 @@
 local function sync_recipes_with_research()
 	local recipes_by_tech = {
 		["electric-energy-accumulators"] = {
-			"bp-battery-charger", 
+			"bp-battery-charger",
 			"bp-battery-discharger",
 			"bp-charged-battery",
 			"bp-advanced-battery",
@@ -11,26 +11,26 @@ local function sync_recipes_with_research()
 			"bp-charged-lithium-sulfur-battery",
 		},
 		["se-space-accumulator"] = {
-			"bp-holmium-battery-charger", 
+			"bp-holmium-battery-charger",
 			"bp-holmium-battery-discharger",
 			"bp-holmium-battery",
 			"bp-charged-holmium-battery",
 		},
 		["se-space-accumulator-2"] = {
-			"bp-naquium-battery-charger", 
+			"bp-naquium-battery-charger",
 			"bp-naquium-battery-discharger",
 			"bp-naquium-battery",
 			"bp-charged-naquium-battery",
 		},
 	}
 
-	for tech, recipes in pairs(recipes_by_tech) do
+	for tech_name, recipes in pairs(recipes_by_tech) do
 		for _, force in pairs(game.forces) do
-			local tech = force.technologies[tech]
+			local tech = force.technologies[tech_name]
 			if tech and tech.researched then
-				for _, recipe in pairs(recipes) do
-					local recipe = force.recipes[recipe]
-					if recipe then 
+				for _, recipe_name in pairs(recipes) do
+					local recipe = force.recipes[recipe_name]
+					if recipe then
 						recipe.enabled = true
 					end
 				end
